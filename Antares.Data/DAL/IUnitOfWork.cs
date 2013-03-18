80223feb22d5b58060b5b97104381c91cb6c972f
Jasen Katagihara <jasen.katagihara@gmail.com>
2013-03-17 22:03:44 -1000

    UnitOfWork

---
+++ Antares.Data/DAL/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace Antares.Data
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        AntaresContext DbContext { get; }
+        int Save();
+    }
+}
